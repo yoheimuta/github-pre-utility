0254b826f2314663b3b886d2ce55b00157f86c89
yoheimuta <yoheimuta@gmail.com>
2015-07-03 08:27:30 +0900

    (refs #7)Created LocalStorage class

--- src/main.js
+++ src/main.js
@@ -53,51 +53,111 @@ ChxGithubPreUtility.Dom = (function() {
     return Dom;
 })();
 
-ChxGithubPreUtility.Controller = (function() {
-    var Controller = {};
-    var collapse_char_length = 80;
+ChxGithubPreUtility.LocalStorage = (function() {
+    function LocalStorage() {
+        this.info = {};
+        this.info.collapse_char_length = 80;
+        this.info.wrap_key_code        = 82; // r key
+        this.info.collapse_key_code    = 84; // t key
+        this.auto_wrap                 = false;
+        this.auto_collapse             = false;
+    }
 
-    // r key
-    var wrap_key_code = 82;
+    LocalStorage.prototype.toObject = function() {
+        return this.info;
+    };
 
-    // t key
-    var collapse_key_code = 84;
-
-    Controller.run = function() {
+    LocalStorage.prototype.getConfig = function(callback) {
+        var that = this;
 
         chrome.runtime.sendMessage({method: "getLocalStorage", key: "config"}, function(res) {
-            if (res && res.data) {
-                var config = JSON.parse(res.data);
-                if (config) {
-                    var length = config.collapse_char_length;
-                    if (length && 0 < length) {
-                        collapse_char_length = length;
-                    }
-
-                    var wrap_key = config.wrap_key_code;
-                    if (wrap_key && 0 < wrap_key) {
-                        wrap_key_code = wrap_key;
-                    }
-
-                    var collapse_key = config.collapse_key_code;
-                    if (collapse_key && 0 < collapse_key) {
-                        collapse_key_code = collapse_key;
-                    }
-                }
+            if (!res || !res.data) {
+                callback(null);
+                return;
             }
 
-            chrome.runtime.sendMessage({method: "getLocalStorage", key: "wrap"}, function(res) {
-                if (res.data != "manual") {
+            var config = JSON.parse(res.data);
+            if (!config) {
+                callback(null);
+                return;
+            }
+
+            var length = config.collapse_char_length;
+            if (length && 0 < length) {
+                that.info.collapse_char_length = length;
+            }
+
+            var wrap_key = config.wrap_key_code;
+            if (wrap_key && 0 < wrap_key) {
+                that.info.wrap_key_code = wrap_key;
+            }
+
+            var collapse_key = config.collapse_key_code;
+            if (collapse_key && 0 < collapse_key) {
+                that.info.collapse_key_code = collapse_key;
+            }
+
+            callback(null);
+        });
+    };
+
+    LocalStorage.prototype.getWrap = function(callback) {
+        var that = this;
+
+        chrome.runtime.sendMessage({method: "getLocalStorage", key: "wrap"}, function(res) {
+            if (res.data != "manual") {
+                that.info.auto_wrap = true;
+            }
+
+            callback(null);
+        });
+    };
+
+    LocalStorage.prototype.getCollapse = function(callback) {
+        var that = this;
+
+        chrome.runtime.sendMessage({method: "getLocalStorage", key: "collapse"}, function(res) {
+            if (res.data != "manual") {
+                that.info.auto_collapse = true;
+            }
+
+            callback(null);
+        });
+    };
+
+    return LocalStorage;
+})();
+
+ChxGithubPreUtility.Controller = (function() {
+    var Controller = {};
+    var storage    = new ChxGithubPreUtility.LocalStorage();
+
+    Controller.run = function() {
+        storage.getConfig(function(err) {
+            if (err) {
+                return;
+            }
+
+            ChxGithubPreUtility.KeydownEvent.startWrap(storage.toObject().wrap_key_code);
+            ChxGithubPreUtility.KeydownEvent.startCollapse(storage.toObject().collapse_key_code, storage.toObject().collapse_char_length);
+
+            storage.getWrap(function(wrap_err) {
+                if (wrap_err) {
+                    return;
+                }
+
+                if (storage.toObject().auto_wrap) {
                     ChxGithubPreUtility.Dom.wrap();
                 }
-                ChxGithubPreUtility.KeydownEvent.startWrap(wrap_key_code);
             });
-
-            chrome.runtime.sendMessage({method: "getLocalStorage", key: "collapse"}, function(res) {
-                if (res.data != "manual") {
-                    ChxGithubPreUtility.Dom.collapse(collapse_char_length);
+            storage.getCollapse(function(collapse_err) {
+                if (collapse_err) {
+                    return;
                 }
-                ChxGithubPreUtility.KeydownEvent.startCollapse(collapse_key_code, collapse_char_length);
+
+                if (storage.toObject().auto_collapse) {
+                    ChxGithubPreUtility.Dom.collapse();
+                }
             });
         });
     };
